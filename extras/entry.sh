8766088c310a1436ff8513c997fff674a96557bb
Jackson Thetford <j.thetford@utexas.edu>
2024-04-27 17:44:06 -0500

    conditional updates

--- extras/entry.sh
+++ extras/entry.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # entry.sh: decides which startup script to run based on an environment variable. Currently supports apptainer and not apptainer (docker)
 
-if [ -z "$_tapisAppId" ]; then
-    # APPTAINER_COMMAND is not set, run startup-tacc.sh
+if [ -n "$_tapisAppId" ]; then
+    echo "Running in tapis environmnet"
     exec /usr/bin/startup-tacc.sh
 else
-    # APPTAINER_COMMAND is set, run startup-tack.sh
     exec /usr/bin/startup.sh
 fi
